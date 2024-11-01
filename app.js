f941d5a31c4142f472274332bec7e159231dad75
535220175_randy <randy@535220175@stu.untar.ac.id>
2024-11-02 03:37:31 +0700

    first commit

--- app.js
+++ app.js
@@ -10,27 +10,40 @@ const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
+//dotnev
 require('dotenv').config();
-
 const admin = require('firebase-admin');
 
-const firebaseConfig = {
+const serviceAccount = {
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newline characters
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
     token_uri: process.env.FIREBASE_TOKEN_URI,
     auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
     client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
 };
 
 admin.initializeApp({
-    credential: admin.credential.cert(firebaseConfig)
+    credential: admin.credential.cert(serviceAccount),  
 });
+
+
+// var admin = require("firebase-admin");
+
+
+// var serviceAccount = require("./web-int-1-firebase-adminsdk-xiyt8-73777d32cf.json");
+
+
+// admin.initializeApp({
+
+//     credential: admin.credential.cert(serviceAccount)
+
+// });
 
 
 const e = require('express');
